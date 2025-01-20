9310cf731d42fa17f1237f0ac17d1fbb13c94e1e
Luis Eduardo <luixst1@gmail.com>
2025-01-20 16:57:04 -0300

    Method to find user by id

--- src/main/java/com/compass/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/compass/demo_park_api/web/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.compass.demo_park_api.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,11 @@ public class UsuarioController {
     public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
         Usuario user = usuarioService.salvar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
     }
 }
