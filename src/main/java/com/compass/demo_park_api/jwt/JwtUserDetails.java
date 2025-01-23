ba5e49a5f2e8f2244efa5fdd02733bd5b3e4982a
Luis Eduardo <luixst1@gmail.com>
2025-01-23 15:26:06 -0300

    Creating JwtUserDetails to manage user authentication and authorization

---
+++ src/main/java/com/compass/demo_park_api/jwt/JwtUserDetails.java
@@ -0,0 +1,24 @@
+package com.compass.demo_park_api.jwt;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import com.compass.demo_park_api.entity.Usuario;
+
+public class JwtUserDetails extends User{
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return  this.usuario.getId();
+    }
+
+    public String getRole() {
+        return this.usuario.getRole().name();
+    }
+
+}
