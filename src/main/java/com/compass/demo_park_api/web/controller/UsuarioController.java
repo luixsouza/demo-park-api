0f6261b83cac8c04b3e6a50050a6a58c22ec5bd0
Luis Eduardo <luixst1@gmail.com>
2025-01-24 14:25:23 -0300

    Access permission for admin to find users

--- src/main/java/com/compass/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/compass/demo_park_api/web/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -60,6 +61,7 @@ public class UsuarioController {
             content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
     })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
