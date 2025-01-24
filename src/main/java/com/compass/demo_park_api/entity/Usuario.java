d305db4e59cf8cb9b4a6e24da7e3d96600e2b054
Luis Eduardo <luixst1@gmail.com>
2025-01-24 12:49:59 -0300

    Implementing and testing the audit system

--- src/main/java/com/compass/demo_park_api/entity/Usuario.java
+++ src/main/java/com/compass/demo_park_api/entity/Usuario.java
@@ -8,9 +8,16 @@ import lombok.Setter;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario implements Serializable {
 
     @Id
@@ -29,15 +36,19 @@ public class Usuario implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
 
+    @CreatedDate
     @Column(name = "data_criacao")
     private LocalDateTime dataCriacao;
 
+    @LastModifiedDate
     @Column(name = "data_modificacao")
     private LocalDateTime dataModificacao;
 
+    @CreatedBy
     @Column(name = "criado_por")
     private String criadoPor;
 
+    @LastModifiedBy
     @Column(name = "modificado_por")
     private String modificadoPor;
 
